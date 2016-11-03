45d8fac303681b5d0148b02199d801d0459e6106
Geoffrey Pitman <leclair566@gmail.com>
2016-11-03 03:17:29 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var extract = require('pdf-text-extract')
 var path = require('path')
 var filePath = path.join(__dirname, './pdf.pdf')
 
+var extract = require('pdf-text-extract')
 // usage --> https://www.npmjs.com/package/pdf-text-extract
 extract(filePath, { splitPages: false}, function (err, text) 
 {
@@ -10,7 +10,10 @@ extract(filePath, { splitPages: false}, function (err, text)
     {
         console.dir(err);
         return;
-    }
+    } 
+    // all ASCII chars shown (e.g. '/n')
     console.dir(text);
+
+    // output contents to file
     fs.writeFile("./pdf.txt", text);
 })
